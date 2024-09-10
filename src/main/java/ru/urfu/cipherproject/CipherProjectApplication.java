5894a65b4923fbe538ad1c5384370f6b21b9bd90
mikhailemv <emelianovmkevg@yandex.ru>
2024-09-10 15:42:51 +0500

    Initial Commit

---
+++ src/main/java/ru/urfu/cipherproject/CipherProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.cipherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CipherProjectApplication.class, args);
+    }
+
+}
